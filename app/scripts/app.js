5fe1c7252ac18f0dc077c46dc31ed904a2227641
Dick Kreisberg <rkreisberg@systemsbiology.org>
2013-03-05 17:18:47 -0800

    switching to test data

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,8 +9,7 @@ define(['queue', 'splitiscope'], function (queue, splitiscope) {
     var Application = {
         initialize: function(){
             queue()
-                .defer(d3.csv,'/test')
-                .defer(d3.csv,'/test1')
+                .defer(d3.json,'http://glados1.systemsbiology.net:3335/svc/data/analysis/rf-leaves/layouts/2013_02_21_ms_ITMI_DF3b_no_NPF_M_FM_3_hilevel.fm_NB_TermCategory_pred_17_12800_10000_1000_4/fiedler/2013_02_21_ms_ITMI_DF3b_no_NPF_M_FM_3_hilevel.fm_NB_TermCategory_pred_17_12800_10000_1000_4.cutoff.0.0.json')
                 .await(function(error, data1, data2){
                     if (error) { errorMsg(error);}
                     console.log(data1);
