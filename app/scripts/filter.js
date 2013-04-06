e17f642c16b925681a7adc64f63ea9031cad956f
Dick Kreisberg <rkreisberg@systemsbiology.org>
2013-04-05 17:33:07 -0700

    abstract the filter/group functionality to work with feature labels

---
+++ app/scripts/filter.js
@@ -0,0 +1,63 @@
+//filter.js
+define([ 
+    'crossfilter' 
+    ], 
+    function(crossfilter) {
+        'use strict';
+
+        var cf_obj,
+               all,
+             filter = {};
+             group = {};
+
+        var Filter = {
+
+            initialize : function(data) {
+                cf_obj = crossfilter(data);
+                all = cf_obj.groupAll();
+            },
+
+            column : function(label) {
+                if arguments.length < 1 return Object.keys(filter);
+                if ( filter[ label ] === undefined ) return false;
+                return true;
+            },
+
+            filterColumn : function( label , property ) { //property is optional
+                if  ( arguments.length < 2 ) property = label;
+                filter[label] = cf_obj.dimension(function(d) { return d[property]; });
+            },
+
+            groupColumn : function( label , property ) { //property is optional
+                if  ( arguments.length < 2 ) property = label;
+                if ( !column( label ) ) Filter.filterColumn(label, property);
+                group[label] = filter[label].group().reduceCount();
+            },
+
+            filterValues : function( label , range ) {
+                if ( !Filter.column(label) ) Filter.filterColumn( label );
+                if ( range.length != 2 || !(_.all(range,_.isNumber))  ) {//filterFunction
+                    filter[label].filterFunction(function(val) { return _.contains(range, val ); } );
+                } else {
+                     filter[label].filterRange( _.map([range[0], range[1]], parseFloat) );
+                }
+            },
+
+            resetFilter : function ( label ) {
+                filter[label].filter(null);
+            },
+
+            getGroups : function( label ) {
+                return _.pluck( classGroup.top(Infinity),'key').sort();
+            },
+
+            getRows : function( label, num ) {
+                if (arguments.length < 2) num = Infinity;
+                if (arguments.length < 1) label = Object.keys(filter)[0];
+                return filter[label].top(num);
+            }
+
+        };
+
+        return Filter;
+});
